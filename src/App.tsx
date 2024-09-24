c8ebc9af601e757368d958f8f9731887124d00b9
Danil L <danillitvinenkod00@gmail.com>
2024-09-24 21:06:18 +0300

    the form is implemented using react-hook-form and yup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,34 @@
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 import styled from "styled-components";
+import { useRef } from "react";
+
+const fieldScheme = yup.object().shape({
+  email: yup
+    .string()
+    .required("Поле обязательно для заполнения")
+    .min(6, "Ошибка! Минимальная длинна поля - 6 символов!")
+    .matches(/@/, 'Неверный формат. Отсутствует символ - "@"')
+    .max(20, "Ошибка! Максимальная длинна поля - 20 символов!"),
+  password: yup
+    .string()
+    .required("Поле обязательно для заполнения")
+    .min(6, "Ошибка! Минимальная длинна поля - 6 символов!")
+    .matches(
+      /[^a-zA-Z0-9\s].*[^a-zA-Z0-9\s]/,
+      "Неверный формат. Пароль должен содержать минимум два спец. символа!"
+    )
+    .matches(
+      /[A-Z].*[A-Z]/,
+      "Неверный формат. Пароль должен содержать минимум две заглавные буквы!"
+    )
+    .max(20, "Ошибка! Максимальная длинна поля - 20 символов!"),
+  confirmPassword: yup
+    .string()
+    .required("Поле обязательно для заполнения")
+    .oneOf([yup.ref("password")], "Пароли должны совподать!"),
+});
 
 const Form = styled.form`
   background-color: var(--color-background-secondary);
@@ -9,6 +39,11 @@ const Form = styled.form`
   border-radius: 10px;
   border: 1px solid var(--color-border);
   width: 500px;
+`;
+
+const ErrorField = styled.div`
+  font-size: 14px;
+  color: red;
 `;
 
 const FormTitle = styled.h1`
@@ -34,6 +69,7 @@ const FormInput = styled.input`
   border-bottom: 1px solid var(--color-text);
   outline: none;
   color: inherit;
+  width: 100%;
 `;
 
 const SubmitButton = styled.button`
@@ -43,33 +79,177 @@ const SubmitButton = styled.button`
   padding: 4px 10px;
   font-weight: 600;
   border-radius: 10px;
+  cursor: pointer;
+
+  &[disabled] {
+    cursor: default;
+    opacity: 0.5;
+  }
 `;
 
+interface CleanFieldButtonProps {
+  value: boolean;
+}
+
+const CleanFieldButton = styled.button<CleanFieldButtonProps>`
+  cursor: pointer;
+  position: absolute;
+  top: 50%;
+  right: 10px;
+  transform: translateY(-50%);
+  font-size: 14px;
+  color: rgba(255, 255, 255, 0.5);
+  opacity: ${(props) => (props.value ? "0" : "1")};
+  visibility: ${(props) => (props.value ? "hidden" : "visible")};
+`;
+
+interface formDataState {
+  confirmPassword: string;
+  email: string;
+  password: string;
+}
+
+const sendFormData = (formData: formDataState) => {
+  console.log(formData);
+};
+
 function App() {
+  const submitRef = useRef<HTMLButtonElement>(null);
+
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    setValue,
+    formState: { errors },
+  } = useForm<formDataState>({
+    defaultValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    mode: "onChange",
+    resolver: yupResolver(fieldScheme),
+  });
+
+  const handleClick = (fieldName: keyof formDataState) => {
+    setValue(fieldName, "");
+  };
+
+  const fieldsError = {
+    email: errors.email?.message,
+    password: errors.password?.message,
+    confirmPassword: errors.confirmPassword?.message,
+  };
+
+  const checkFields = () => {
+    if (
+      getValues("email") !== "" &&
+      !fieldsError.email &&
+      getValues("password") !== "" &&
+      !fieldsError.password &&
+      getValues("confirmPassword") !== "" &&
+      !fieldsError.confirmPassword
+    ) {
+      submitRef.current?.focus();
+    }
+  };
+
+  checkFields();
+
   return (
     <>
-      <Form>
+      <Form onSubmit={handleSubmit(sendFormData)}>
         <FormTitle>Register Form</FormTitle>
         <FormRow>
           <FormLabel htmlFor="email">Email</FormLabel>
-          <FormInput type="text" id="email" name="email" />
+          <div
+            style={{
+              width: "100%",
+              position: "relative",
+            }}
+          >
+            <FormInput
+              {...register("email")}
+              type="text"
+              id="email"
+              name="email"
+            />
+            <CleanFieldButton
+              value={getValues("email") === ""}
+              onClick={() => handleClick("email")}
+            >
+              X
+            </CleanFieldButton>
+          </div>
+
+          {fieldsError.email && <ErrorField>{fieldsError.email}</ErrorField>}
         </FormRow>
 
         <FormRow>
           <FormLabel htmlFor="password">Password</FormLabel>
-          <FormInput type="password" id="password" name="password" />
+          <div
+            style={{
+              width: "100%",
+              position: "relative",
+            }}
+          >
+            <FormInput
+              {...register("password")}
+              type="password"
+              id="password"
+              name="password"
+            />
+            <CleanFieldButton
+              value={getValues("password") === ""}
+              onClick={() => handleClick("password")}
+            >
+              X
+            </CleanFieldButton>
+          </div>
+
+          {fieldsError.password && (
+            <ErrorField>{fieldsError.password}</ErrorField>
+          )}
         </FormRow>
 
         <FormRow>
-          <FormLabel htmlFor="confirm-password">Confirm password</FormLabel>
-          <FormInput
-            type="password"
-            id="confirm-password"
-            name="confirm-password"
-          />
+          <FormLabel htmlFor="confirmPassword">Confirm password</FormLabel>
+          <div
+            style={{
+              width: "100%",
+              position: "relative",
+            }}
+          >
+            <FormInput
+              {...register("confirmPassword")}
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+            />
+            <CleanFieldButton
+              value={getValues("confirmPassword") === ""}
+              onClick={() => handleClick("confirmPassword")}
+            >
+              X
+            </CleanFieldButton>
+          </div>
+
+          {fieldsError.confirmPassword && (
+            <ErrorField>{fieldsError.confirmPassword}</ErrorField>
+          )}
         </FormRow>
 
-        <SubmitButton>Register</SubmitButton>
+        <SubmitButton
+          ref={submitRef}
+          disabled={
+            !!fieldsError.email ||
+            !!fieldsError.password ||
+            !!fieldsError.confirmPassword
+          }
+        >
+          Register
+        </SubmitButton>
       </Form>
     </>
   );
