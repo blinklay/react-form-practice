8f58cf356a54455685dabdb3515eece166ece553
Danil L <danillitvinenkod00@gmail.com>
2024-09-24 16:50:50 +0300

    Implemented form validation without using technologies

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import FormLayout from "./components/FormLayout";
 import React, { useState } from "react";
+import { formDataState } from "./interfaces/formDataState";
 
-interface formDataState {
-  email: string;
-  password: string;
-  confirmPassword: string;
-}
+const sendFormData = (formData: formDataState) => {
+  console.log("Form Data >>>>> ", formData);
+};
 
 const App: React.FC = () => {
   const [formData, setFormData] = useState<formDataState>({
@@ -14,16 +13,97 @@ const App: React.FC = () => {
     confirmPassword: "",
   });
 
-  const changeField = ({ target }) => {
+  const [errorState, setErrorState] = useState<formDataState>({
+    email: null,
+    password: null,
+    confirmPassword: null,
+  });
+
+  const [formSended, setFormSended] = useState<boolean>(false);
+
+  // Функция проверки заполненности полей и отсутствия ошибок
+  const checkFields = (): boolean => {
+    if (
+      formData.email === "" ||
+      formData.password === "" ||
+      formData.confirmPassword === ""
+    )
+      return true;
+    if (errorState.email || errorState.password || errorState.confirmPassword)
+      return true;
+
+    return false;
+  };
+
+  const changeField = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({
       ...formData,
       [target.name]: target.value,
     });
+
+    let errorMessage = null;
+    if (target.name === "confirmPassword") {
+      if (!(formData.password === target.value)) {
+        errorMessage = "Значение не совпадает с значением поля Пароль!";
+      }
+    }
+
+    setErrorState({ ...errorState, [target.name]: errorMessage });
+  };
+
+  const onBlurField = ({ target }: React.FocusEvent<HTMLInputElement>) => {
+    let errorMessage = null;
+
+    if (target.name === "email") {
+      if (target.value.length < 6) {
+        errorMessage = "Ошибка! Поле менее 6-ти символов!";
+      } else if (!/@/.test(target.value)) {
+        errorMessage = "Ошибка! В поле отсутствует символ - '@'";
+      }
+    }
+
+    if (target.name === "password") {
+      if (target.value.length < 6) {
+        errorMessage = "Ошибка! Поле менее 6-ти символов!";
+      } else if (target.value.length > 20) {
+        errorMessage = "Ошибка! Поле более 20-ти символов!";
+      } else if (!/[A-Z].*[A-Z]/.test(target.value)) {
+        errorMessage =
+          "Ошибка! Пароль должен содержать минимум две заглавных буквы!";
+      } else if (!/[^a-zA-Z0-9\s].*[^a-zA-Z0-9\s]/.test(target.value)) {
+        errorMessage =
+          "Ошибка! Пароль должен содержать минимум два спец. символа!";
+      }
+    }
+
+    setErrorState({ ...errorState, [target.name]: errorMessage });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    sendFormData({
+      email: formData.email,
+      password: formData.password,
+      confirmPassword: formData.confirmPassword,
+    });
+
+    setFormSended(true);
   };
 
   return (
     <>
-      <FormLayout changeField={changeField} formData={formData} />
+      <FormLayout
+        changeField={changeField}
+        formData={formData}
+        errorState={errorState}
+        onBlurField={onBlurField}
+        disabledSubmit={checkFields()}
+        setFormData={setFormData}
+        setErrorState={setErrorState}
+        handleSubmit={handleSubmit}
+        formSended={formSended}
+      />
     </>
   );
 };
--- src/components/FormLayout.tsx
+++ src/components/FormLayout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { formDataState } from "../interfaces/formDataState";
 
 const Form = styled.form`
   background-color: var(--color-background-secondary);
@@ -10,6 +11,7 @@ const Form = styled.form`
   border-radius: 10px;
   border: 1px solid var(--color-border);
   width: 500px;
+  position: relative;
 `;
 
 const FormTitle = styled.h1`
@@ -30,11 +32,12 @@ const FormLabel = styled.label`
 const FormInput = styled.input`
   background-color: transparent;
   font-size: 18px;
-  padding: 5px;
+  padding: 5px 30px 5px 5px;
   border: none;
   border-bottom: 1px solid var(--color-text);
   outline: none;
   color: inherit;
+  width: 100%;
 `;
 
 const SubmitButton = styled.button`
@@ -44,49 +47,166 @@ const SubmitButton = styled.button`
   padding: 4px 10px;
   font-weight: 600;
   border-radius: 10px;
+  cursor: pointer;
+  &[disabled] {
+    opacity: 0.5;
+    cursor: default;
+  }
 `;
-const FormLayout: React.FC = ({
+
+interface CleanFieldProps {
+  value: string | null;
+}
+
+const CleanField = styled.button<CleanFieldProps>`
+  position: absolute;
+  top: 50%;
+  right: 10px;
+  transform: translateY(-50%);
+  color: rgba(255, 255, 255, 0.5);
+  cursor: pointer;
+  opacity: ${({ value }) => (value ? "1" : "0")};
+  visibility: ${({ value }) => (value ? "visible" : "hidden")};
+`;
+
+const ErrorField = styled.div`
+  font-size: 14px;
+  color: red;
+`;
+
+const SuccessMessage = styled.div`
+  align-self: center;
+  color: green;
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+`;
+
+interface IFormLayout {
+  changeField: ({ target }: React.ChangeEvent<HTMLInputElement>) => void;
+  onBlurField: ({ target }: React.FocusEvent<HTMLInputElement>) => void;
+  errorState: formDataState;
+  formData: formDataState;
+  disabledSubmit: boolean;
+  setFormData: (formData: formDataState) => void;
+  setErrorState: (formData: formDataState) => void;
+  handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+  formSended: boolean;
+}
+
+const FormLayout: React.FC<IFormLayout> = ({
   changeField,
   formData: { email, password, confirmPassword },
+  errorState,
+  onBlurField,
+  disabledSubmit,
+  setFormData,
+  setErrorState,
+  handleSubmit,
+  formSended,
 }) => {
   return (
     <>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <FormTitle>Register Form</FormTitle>
         <FormRow>
           <FormLabel htmlFor="email">Email</FormLabel>
-          <FormInput
-            onChange={changeField}
-            type="text"
-            id="email"
-            value={email}
-            name="email"
-          />
+          <div
+            style={{
+              width: "100%",
+              position: "relative",
+            }}
+          >
+            <FormInput
+              onChange={changeField}
+              onBlur={onBlurField}
+              type="text"
+              id="email"
+              value={email ?? ""}
+              name="email"
+            />
+            <CleanField
+              type="button"
+              value={email}
+              onClick={() => {
+                setFormData({ password, confirmPassword, email: "" });
+                setErrorState({ ...errorState, email: null });
+              }}
+            >
+              x
+            </CleanField>
+          </div>
+          {errorState.email && <ErrorField>{errorState.email}</ErrorField>}
         </FormRow>
 
         <FormRow>
           <FormLabel htmlFor="password">Password</FormLabel>
-          <FormInput
-            onChange={changeField}
-            type="password"
-            id="password"
-            value={password}
-            name="password"
-          />
+          <div
+            style={{
+              width: "100%",
+              position: "relative",
+            }}
+          >
+            <FormInput
+              onBlur={onBlurField}
+              onChange={changeField}
+              type="password"
+              id="password"
+              value={password ?? ""}
+              name="password"
+            />
+            <CleanField
+              value={password}
+              onClick={() => {
+                setFormData({ email, confirmPassword, password: "" });
+                setErrorState({ ...errorState, password: null });
+              }}
+              type="button"
+            >
+              x
+            </CleanField>
+          </div>
+
+          {errorState.password && (
+            <ErrorField>{errorState.password}</ErrorField>
+          )}
         </FormRow>
 
         <FormRow>
           <FormLabel htmlFor="confirm-password">Confirm password</FormLabel>
-          <FormInput
-            onChange={changeField}
-            type="password"
-            id="confirm-password"
-            value={confirmPassword}
-            name="confirmPassword"
-          />
+          <div
+            style={{
+              width: "100%",
+              position: "relative",
+            }}
+          >
+            <FormInput
+              onChange={changeField}
+              type="password"
+              id="confirm-password"
+              value={confirmPassword ?? ""}
+              name="confirmPassword"
+            />
+            <CleanField
+              value={confirmPassword}
+              onClick={() => {
+                setFormData({ email, password, confirmPassword: "" });
+                setErrorState({ ...errorState, confirmPassword: null });
+              }}
+              type="button"
+            >
+              x
+            </CleanField>
+          </div>
+
+          {errorState.confirmPassword && (
+            <ErrorField>{errorState.confirmPassword}</ErrorField>
+          )}
         </FormRow>
 
-        <SubmitButton>Register</SubmitButton>
+        <SubmitButton disabled={disabledSubmit}>Register</SubmitButton>
+        {formSended && <SuccessMessage>Форма отправлена!</SuccessMessage>}
       </Form>
     </>
   );
